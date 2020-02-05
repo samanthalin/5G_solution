01ae65bebbd8191a04f99540bd8443bafbc41d0d
Zyxel <zyxel@TWPCNT03103-01.local>
2020-02-05 18:20:03 +0800

    final final

--- library/js/solutions/5G_solution_page/style.js
+++ library/js/solutions/5G_solution_page/style.js
@@ -93,7 +93,10 @@ $(document).ready(function(){
 	$('.sn-nsa .button').click(function() {
 		$('.sn-nsa-hidden').toggle();
 		$('.sn-nsa-hidden').addClass('animated');
-		$('.switch a[href^="#standalone"]').addClass('active');
+		$('.switch a').removeClass('active');
+		$('.switch a[href="#standalone"]').addClass('active');
+		$('#non-standalone').hide();
+		$('#standalone').show();
 		return false;
 	});
 	$('.switch a').click(function() {
